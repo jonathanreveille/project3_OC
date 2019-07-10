b2dbf0aab2e6aa35b46bdeb7736ae05bab6831fa
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-07-10 12:05:29 +0200

    Module is created, no action done yet

--- demo/items.py
+++ demo/items.py
@@ -6,3 +6,32 @@ the items will dispear from the map and appear in the inventory of
 Mcgaver,
 Mcgavers (hero) should obtain a way to pick up and save up thos items 
 """
+
+import random
+
+
+class Items:
+    pass
+    """ this class will create objects that will represent the items for the hero
+    on the gameboard to pick-up,
+    those items are going to be positionned randomly on self.gameboard.passages 
+    with the help of the module random (to import) """
+
+    #def __init__(self, needle, tube, ether):
+        
+        #self.needle = None
+        #self.tube = None
+        #self.ether = None
+
+    #def sample():
+        # On créer une liste 'random_position' vide
+        #random_position = []
+        # on utilise la fonction random.sample(population qui est une séquence donc self.passages, k=3 on tire 3 positions)
+        #random_position = random.sample(self.gameboard.passages, k=3)
+        #importer GameBoard dans ce module
+
+        #self.load_from_file()
+
+       # on tire 3 positions dans self.passages
+
+        #random.sample(self.passages, k=3)
