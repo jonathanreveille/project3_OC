048bc9a8d1f4b1b2c78093a837baac9379d4a8d2
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-07-10 10:08:49 +0200

    Module items.py created for the game, will write pseudo code first

---
+++ demo/items.py
@@ -0,0 +1,8 @@
+""" 
+This module will represent all different tools that McGaver
+can pick up in the maze.
+The items will have random positions on the self.passages of the labyrinth,
+the items will dispear from the map and appear in the inventory of 
+Mcgaver,
+Mcgavers (hero) should obtain a way to pick up and save up thos items 
+"""
