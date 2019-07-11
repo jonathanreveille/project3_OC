42dc57f47036b43c7db91277c16993dadb6536c4
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-07-11 16:03:24 +0200

    Created class items,and 3 objects of Items

--- demo/items.py
+++ demo/items.py
@@ -10,27 +10,40 @@ Mcgavers (hero) should obtain a way to pick up and save up thos items
 import random
 
 
+
 class Items:
-    pass
+
     """ this class will create objects that will represent the items for the hero
     on the gameboard to pick-up,
     those items are going to be positionned randomly on self.gameboard.passages 
     with the help of the module random (to import) """
 
-    #def __init__(self, needle, tube, ether):
-        
-        #self.needle = None
-        #self.tube = None
-        #self.ether = None
+    def __init__(self):
+        """ construit un objet item de la classes Items """
+        self.items = ""
+        self.position = None
 
-    #def sample():
+needle = Items()
+
+ether = Items()
+
+tube = Items()
+
+
+
+    #def sample(self): PUT IT IN GAMEBOARD PERHAPS 
         # On créer une liste 'random_position' vide
+
         #random_position = []
         # on utilise la fonction random.sample(population qui est une séquence donc self.passages, k=3 on tire 3 positions)
-        #random_position = random.sample(self.gameboard.passages, k=3)
+        ##random_position = []
+        #random.sample(self.passages, k=3)
+        ##gameboard.load_from_file()
+
+
         #importer GameBoard dans ce module
 
-        #self.load_from_file()
+        
 
        # on tire 3 positions dans self.passages
 
