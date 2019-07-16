5add6809868feaaf6f2e506d46f145cf9a9e2210
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-07-16 16:55:08 +0200

    updated the load_from_file method with the random.sample function for the items

--- demo/gameboard.py
+++ demo/gameboard.py
@@ -1,6 +1,8 @@
 """ this module will allow us to determine the length (and width) of the gameboard (map) """
 
+from random import sample
 from demo.position import Position
+import random
 
 
 class GameBoard():
@@ -18,6 +20,9 @@ class GameBoard():
         self.maze = []
         self.hero = None
 
+        self.items = []
+        self.random_pos= []
+
 
     def load_from_file(self):
         """ This method will load map from a file """
@@ -26,7 +31,7 @@ class GameBoard():
         # cette valeurs en integers permettra au programme de comprendre le nombre de ligne et de colonne
         self.width = int()
         self.length = int()
-    
+
         #Nous allons charger le labyrinthe en fichier texte se situant dans /levels/..., on créer la varible f pour la méthode
             # on décide de strip notre ligne de texte du fichier f pour créer un index pour chaque caractère présent
             # on demande ensuite au programme de lire notre texte : ligne par ligne et de la transformer en liste.
@@ -34,7 +39,6 @@ class GameBoard():
         with open('levels/maze1.txt') as f:
             maze = [line.strip("") for line in f.readlines() if line.strip()]
             maze = list(maze)
-
         #Pour chaque numéro de ligne(x), et ligne dans maze
             #Pour chaque numéro de colonne(y), et colonne dans maze
         for n_lines,lines in enumerate(maze):
@@ -43,7 +47,7 @@ class GameBoard():
                 pos = Position(n_colonne, n_lines)
                 # Si nous avons dans colonne le caractere egale a ".", ajoute-le dans la liste self.passages
                 if colonne  == ".":
-                    self.passages.append(pos)
+                    self.passages.append(pos)                    
                 # Si nous avons dans colonne le caractere egale a "S",  ajoute-le dans la liste self.passages
                 if colonne == "S":
                     self.passages.append(pos)
@@ -52,12 +56,21 @@ class GameBoard():
                 if colonne == "G":
                     self.passages.append(pos)
                     self.goal = pos
+                #if colonne == "I":
+                 #   self.passages.append(pos)
+                  #  self.items = pos
                 # Si nous avons dans colonne le caractere egale a "W",  ajoute-le dans la liste self.walls
                 elif colonne == "W" : #It's a wall
                     self.walls.append(pos)
-            # on ajoute à width et lenght une colonne et ligne en '+'
+
+                   # self.random_pos = sample(self.passages, k=3)
+                    #self.passages.append(self.random_pos)
+            
+            # on ajoute à width le nombre de colonne +1, ainsi que pour length on ajoute le nombre de n_lines +1 
             self.width = n_colonne +1
             self.length = n_lines +1
+
+        self.random = random.sample(self.passages, k=len(self.passages))
 
 
     def __str__(self):
@@ -69,7 +82,6 @@ class GameBoard():
 
         for line in range(self.length):
             for colonne in range(self.width):
-
                 pos = Position(colonne, line)
                 # Verifier si c'est la position de McGaver
                 if pos == self.hero.position:
@@ -78,14 +90,22 @@ class GameBoard():
                     list_str.append("S")
                 elif pos == self.goal:
                     list_str.append("G")
+                elif pos in self.items:
+                    list_str.append("I")
                 elif pos in self.passages:
                     list_str.append(".")
                 elif pos in self.walls:
                     list_str.append("W")
-                    
+                     
             list_str.append("\n")
 
         return "".join(list_str)
+
+
+    def add_items(self, item):
+        """ method that adds item on the map """
+        item.position = self.random.pop()
+        self.items.append(item)
 
 
     def __contains__(self, position):
