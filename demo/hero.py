6d8d26c1946a2dc58a8bfe5d9cdd57d7ff37f68b
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-08-14 14:18:13 +0200

    Modified def move method, focussed on if hero is on item position, add item to hero's bag and remove Item symbol off the maze. Encouter issue because it does not remove the I the hero just passed over. Need to fix the bug. When using .remove(), the __eq__ does not work anymore... it worked once only, after bugs all the time

--- demo/hero.py
+++ demo/hero.py
@@ -8,7 +8,7 @@ from demo.mouvement import Mouvement
 from demo.gameboard import GameBoard
 from demo.gamewon import GameWon
 from demo.items import Item
-
+from demo.gameover import GameOver
 
 class Hero:
     """ This class will define the position of 
@@ -21,40 +21,33 @@ class Hero:
         self.position = gameboard.start
         self.gameboard = gameboard
         self.gameboard.hero = self
-        self.items = []
+        self.bag = []
         
 
     def move(self, travel):
         """ Method that allows our hero to move on the map from its position to a new position """
-        
+
         new_position = self.position + travel
 
-        if new_position in self.gameboard:
+        if new_position in self.gameboard.items:
             self.position = new_position
+            #del self.gameboard.items[0]
+            self.gameboard.items.pop()
+            #self.gameboard.items.remove('I')
+            self.bag.append("I")
+            print(self.bag)
 
-        if self.position == self.gameboard.goal:
-            raise GameWon ("you made it!")
+        elif new_position in self.gameboard.passages:
+            self.position = new_position
+        
+        elif new_position in self.gameboard.walls:
+            return self.position
+        
+        elif new_position in self.gameboard.goal:
+            self.position = new_position
+        
+        elif self.position == self.gameboard.goal and len(self.bag)==3:
+            raise GameWon ("you made it Morty ! you have just killed the guard!")
 
-        # maybe add here the method to pick up weapons 
-
-
-    #WORKING HERE NOW
-    def store_items(self):
-        """ method where the hero can keep his items """
-        # hero's inventory. If hero's inventory == 3: 
-        # raise 'You just build a weapon'
-
-        # creating empty list to take in the items when hero finds it in labyrinth
-        self.items = []
-        # compteur while loop
-        i = 0
-        while i <= 3: # because only 3 items
-            #if hero's position is on self.items.position in self.gameboard.passages:
-            if self.position == self.items in self.gameboard.passages:
-                # add self.items to the list
-                self.items.append(self.items)
-                i += 1
-
-        #if bag != len(2) is False:
-
-        #if self.position == 
+        else:
+            raise GameOver ("you just died...try again")
