55026b55b8bbb3cbe4a375ba1853a4495e657f83
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-08-29 09:13:27 +0200

    created add_to_bag() method to the class, picking up items is working

--- demo/hero.py
+++ demo/hero.py
@@ -30,36 +30,65 @@ class Hero:
 
         if new_position in self.gameboard.items:
             self.position = new_position
-            #self.gameboard.items.remove('I') # that is the one to keep, but AttributeError occurs
-            self.gameboard.items.remove([0])
-            #self.gameboard.items.pop(0)
-            #del self.gameboard.items[0]
-            self.bag.append("I")
-            print(self.bag)
+            self.add_to_bag()
+
+        elif new_position in self.gameboard.goal:
+            #self.position = new_position
+            self.level_achieved()
 
         elif new_position in self.gameboard.passages:
             self.position = new_position
-        
+
         elif new_position in self.gameboard.walls:
             return self.position
+
+
+    def add_to_bag(self):
+        """method that allows you to take the item from gameboard, and put it into our bag"""
         
-        elif new_position in self.gameboard.goal:
-            self.position = new_position
-                
-        elif new_position in self.gameboard.goal:
-            self.position = new_position
-            
-            #check if hero has 3 items
-            #if self.bag ==3:
-                #raise 
-            
-        elif self.position in self.gameboard.goal and len(self.bag) == 3:
-            raise GameWon("you made it Morty ! you have just killed theguard!")
-
-        elif  self.position ==  self.gameboard.goal and len(self.bag)!= 3:
-            raise GameOver("you forgot an item on the map, you just died...try again")
+        self.gameboard.items.remove([0])
+        self.bag.append("I")
+        print(self.bag)
+        return ("you just found an item")
 
 
+    def level_achieved(self):
+        """check if the hero has 3 items to win the game """
+
+        if len(self.bag) == 3:
+            raise GameWon("you made it McGaver! freedom")
+        elif len(self.bag) != 3:
+            raise GameOver("you must have forgotten an item ! try again...")
+
+
+    #def win_game(self):
+   # """method that checks if the length of self.bag is 3, if true :
+    #raise GameWon (message)
+   # false : raise GameLost (message) """
+    #    
+       # if len(self.bag) == 3:
+        #    if True:
+          #      raise GameWon("you made it Morty ! you have just killed the guard!")
+          #  print("it should launch GameWon")
+           # else:
+           #     raise GameOver("you forgot an item on the map, you just died...try again")
+            #    print("it shoudl launch GameLost")
+
+
+       # """ Allows the hero to add items into his bag when he passes over it"""
+#
+ #      # new_position = self.position + travel
+
+ #      # if new_position == self.gameboard.items:
+ #   #     self.position = new_position
+        #    #self.gameboard.items.remove('I')#([0])
+  #      #    self.bag.append("I")
+   #    #     print(self.bag)
+
+  #          #self.gameboard.items.remove('I') # that is the one to keep, but AttributeError occurs
+  #          #self.gameboard.items.pop(0)
+  #          #del self.gameboard.items[0]
+  #          #self.bag.append("I")
 
         #    raise GameWon ("you made it Morty ! you have just killed theguard!"
             #if  len(self.bag)==3:
