770788ac46a7a692873536a667c70f9ccc5d92f4
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-09-05 13:19:00 +0200

    added  def main(): pass /  if __name__=__main_

--- demo/gameover.py
+++ demo/gameover.py
@@ -6,3 +6,10 @@ GameOver once the hero has reached the exit of the maze, but does not have 3 ite
 class GameOver(Exception):
     """method that shows us when the hero does not have 3 items to build the seringue"""
     pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
