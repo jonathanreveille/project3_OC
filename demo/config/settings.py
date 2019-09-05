f1e3b309e14ff0fe984cf82cef9784e50793c95e
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-09-05 14:15:23 +0200

    created a file with settings.py, images folder and init.py

---
+++ demo/config/settings.py
@@ -0,0 +1,13 @@
+
+# Configs that can be change in an easy way from here
+# Capital letters because they are import "CONSTANTS" for the game
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+WIDTH = 1280
+HEIGHT = 720
+
+BACKGROUND = str(BASE_DIR/"images"/'background.png')
+
