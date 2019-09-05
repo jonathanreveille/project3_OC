112ac22a1292dd39437508c218f4559369a89a15
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-09-05 16:54:26 +0200

    created 2 Class : MacGyver and Game. MacGyver has 2  methods  'def updates' and 'def keyboard events' to track down mouvement from user to the game. Game is created to launch game, quit game, and update  screen and background after user did action

--- pygamemain.py
+++ pygamemain.py
@@ -1,34 +1,144 @@
 #! /usr/bin/env python3
 # coding : utf-8 
+""" Module for pygame interface"""
 
 import pygame as pg
 from demo.config import settings
 
 
+class MacGyver(pg.sprite.Sprite): #Classe d'héritage de Sprite
+
+    def __init__(self): #Initializer
+        super().__init__()  #Appeller la méthode de  sprit  init elle-même 
+        self.image = pg.image.load(settings.HERO).convert() #l'image avec le convert
+        self.rect = self.image.get_rect() # la position de MacGyver. get_rect = c'est le rectangle de mon image et sa position
+
+    def updates(self):
+        self._process_keyboard_events()
+    
+
+    def _process_keyboard_events(self):
+        """ method that captures events from user and do action for game"""
+        
+        for event in pg.event.get():
+            if event == pg.KEYDOWN and pg.event.key == pg.K_UP:
+                print("up")
+                self.rect.move_ip(0, -settings.VELOCITY)
+            
+            elif event == pg.KEYDOWN and pg.event.key == pg.K_DOWN:
+                self.rect.move_ip(0, +settings.VELOCITY)
+            
+            elif event == pg.KEYDOWN and pg.event.key == pg.K_LEFT:
+                self.rect.move_ip(-settings.VELOCITY, 0)
+            
+            elif event == pg.KEYDOWN and pg.event.key == pg.K_RIGHT:
+                self.rect.move_ip(settings.VELOCITY, 0)
+                
+
+
 class Game:
     """ Class that represents the game for pygame"""
 
+    def __init__(self): 
+        """ All elements that are needed for the game"""
 
-    def __init__(self): # initializer
-        pg.init()
-        self.screen = pg.display.set_mode((settings.WIDTH,settings.HEIGHT))
-        self.background = pg.image.load(settings.BACKGROUND)
+        # Initializer pygame
+        pg.init() #No screen appears..
+        
+        self.screen = pg.display.set_mode((settings.WIDTH,settings.HEIGHT)) #Building the window
+        self.background = pg.image.load(settings.BACKGROUND).convert() #Building the background
+        self.screen.blit(self.background, (0,0))# Putting the background on the screen surface with blit(). On vient de copier le background sur la surface. Blit veut dire : copier une image sur une surface (self.screen ci-dessus dans notre cas)
+        
+    
+        self.sprites = pg.sprite.RenderUpdates()
+        self.sprites.add(MacGyver())
+
+       # self.screen.blit(self.macgyver, (0,0))  # sur la position de départ
+
+
+        pg.display.update() #we always update
+        self.running = False  #to know if game runs or not in our while loop, we will use boolean values T or F
+
+        pg.display.set_caption("MacGyver")
+
+
 
 
     def start(self): # method to launch the game
-        pass
+        """Method that launches the game"""
+       
+        self.running = True
+       
+
+        # loop for game launch
+        while self.running:
+            
+            self._process_quit_events() #process de fermeture  de l'écran de jeu
+
+            #1. Couvrir l'espace de jeu avec le background
+            self.sprites.clear(self.screen, self.background) #  on clear notre self.screen, et on pause le bg par dessus
+            
+            #2. Appeler  la update sur toute  les sprites
+            self.sprites.update()
+            
+            #3. Redessiser  ce qui doit l'être
+            updated_sprites = self.sprites.draw(self.screen)
+            pg.display.update(updated_sprites)
 
 
 
+    def _process_quit_events(self):
+        """ method that quits the game when the user press the exit button on game window"""
+        for _ in pg.event.get(pg.QUIT):
+            self.running = False
+
+
+
+
+
+
+
+
+            #for event in pg.event.get():
+                #if event.type == QUIT:
+                #    self.running = False
+
+            #    elif pg.event.type == pg.KEYDOWN and event.key == K_ESCAPE:
+            #    self.running = False
+
+            #    elif event.type == KEYDOWN and event.key == K_UP:
+            #       hero.move()
+
+            #   elif event.type == KEYDOWN and event.key == K_DOWN:
+            #       hero.move()
+
+                #elif event.type == KEYDOWN and event.key == K_LEFT:
+                    #hero.move()
+
+                #elif event.type == KEYDOWN and event.key == K_RIGHT:
+                    #hero.move()
+                
+                #pg.display.update()
 
 
 def main():
     game = Game()
     game.start()
 
- 
+if __name__ == "__main__":
+    main()
 
 
+#1- It works, but no need anymore for now in def start from class Game :
+        #self.running = True
+        #while self.running:
+            #pg.event.pump()
+
+            #response = input('Do you want to quit the game?')
+            #if response == "quit" or " quit":
+                #self.running = False
+
+#2- Following pygamechimp tutorial
 #def main():
     #Example from the tutorial
     #Initialize everything
@@ -51,6 +161,11 @@ def main():
     #screen.blit(background,(0, 0))
     #pygame.display.flip()
 
+#CONVERT  
+        #self.macgyver = pg.image.load(settings.HERO).convert() # .convert_alpha() : if MacGyver png file is already transparent, to make bg transparent
+        #self.macgyver.set_colorkey(settings.COULEUR) Si je trouve une image jpeg du hero avec fond de couleur
+        
 
-if __name__ == "__main__":
-    main()
+#pg.display.FLIP() VS. pg.display.UPDATE()
+#pg.display.flip() c'est une mise à jour de tout l'écran, un peu différent de update. Car quand on MOVE, c'est seulement une 
+#case qui change. flip() me permet de mettre à jour toute la surface. Update rafraichi uniquement les mouvements
