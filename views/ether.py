bde2e8d63dfc822dd84e829cc651b69864ddc66b
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-09-12 12:13:57 +0200

    all changes to make it work again

--- views/ether.py
+++
@@ -1,9 +0,0 @@
-
-
-class MacGyver(pg.sprite.Sprite): #Classe d'héritage de Sprite
-    """ class to add hero on game, record  his moves  and updates"""
-
-    def __init__(self): #Initializer
-        super().__init__()  #Appeller la méthode de  sprit  init elle-même 
-        self.image = pg.image.load(settings.HERO).convert() #l'image avec le convert
-        self.rect = self.image.get_rect() 
--- views/tube.py
+++
@@ -1,4 +0,0 @@
-import pygame
-import .demo.config import settings
-
-
