319b0916b128afaa791dcfa4ab1e55c017a88add
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-09-17 11:04:04 +0200

    Hero stays on the map and the screen

--- macgyver/views/macgyver.py
+++ macgyver/views/macgyver.py
@@ -4,21 +4,27 @@
 we control his directions according to moves"""
 
 import pygame as pg
-from config.settings import HERO, VELOCITY
-
+from config.settings import HERO, VELOCITY, SPRITE_WIDTH, SPRITE_HEIGHT
+from macgyver.demo.mouvement import right, left, up, down
 
 class MacGyver(pg.sprite.Sprite): #Classe d'héritage de Sprite
     """ class to add hero on game, record  his moves  and updates"""
 
-    def __init__(self): #Initializer
-        super().__init__()  #Appeller la méthode de  sprit  init elle-même 
-        self.image = pg.image.load(HERO).convert() #l'image avec le convert
+    def __init__(self, hero): #Initializer
+        
+        super().__init__()  #calling sprites class to initialize object, use of super().__init__()
+        self.image = pg.image.load(HERO).convert_alpha() #loading image
         self.rect = self.image.get_rect() # la position de MacGyver. get_rect = c'est le rectangle de mon image et sa position
+        self.hero = hero
 
 
     def update(self):
         """ Methods that manages and tracks all updates from events"""
         self._process_keyboard_events()
+        self.rect.x = self.hero.position.x * SPRITE_WIDTH
+        self.rect.y = self.hero.position.y * SPRITE_HEIGHT
+
+        # sur la base de la position de McG
     
 
     def _process_keyboard_events(self):
@@ -26,19 +32,24 @@ class MacGyver(pg.sprite.Sprite): #Classe d'héritage de Sprite
         
         for event in pg.event.get():
             if event.type == pg.KEYDOWN and event.key == pg.K_UP:
-                self.rect.move_ip(0, -VELOCITY)
+                #self.rect.move_ip(0, -VELOCITY)
+                self.hero.move(up)
             
             elif event.type == pg.KEYDOWN and event.key == pg.K_DOWN:
-                self.rect.move_ip(0, +VELOCITY)
-            
+                #self.rect.move_ip(0, +VELOCITY)
+                self.hero.move(down)
             elif event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
-                self.rect.move_ip(-VELOCITY, 0)
+                #self.rect.move_ip(-VELOCITY, 0)
+                self.hero.move(left)
             
             elif event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
-                self.rect.move_ip(VELOCITY, 0)
+                #self.rect.move_ip(VELOCITY, 0)
+                self.hero.move(right)
 
-                # il bougera seulement dans passage
-    def _limits_moves(self):
-        pass #TO DO !
+
+    def _limits_moves(self, position):
+        """method to make the hero stay on the screen"""
+        return self.hero in self.gameboard.passages
+
     
 # Ajouter la limite de MacGyver dans l'écran
