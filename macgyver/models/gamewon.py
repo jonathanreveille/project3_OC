9e9793d8778ed9200e13be65155bd21bf751e7f8
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-09-19 18:09:05 +0200

    changed name of file from demo to models

---
+++ macgyver/models/gamewon.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# coding : utf-8 
+
+""" This module was created to allow pop up the message 
+Game won once the hero has reached the exit of the maze with 3 items """
+
+class GameWon(Exception):
+    pass
+
+def main():
+    print("you are in gamewon.py")
+
+if __name__ == "__main__":
+    main()
---
+++ macgyver/models/items.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+# coding : utf-8 
+
+""" This module will represent all different items on the map for McGaver"""
+
+class Item:
+
+    """ This class will create objects that are items in the game,
+    those items are going to be positionned randomly on the map """
+
+    def __init__(self, name):
+        """ constructor for item Class objects"""
+        self.name = name
+        self.position = None
+
+    def __repr__(self):
+        """ method that allow us to see what data has an object from Item Class"""
+        return f"Item(position={self.position},name= ({self.name})"
+
+    def __str__(self):
+        return self.name[0]
+
+    def __eq__(self, obj):
+        """ method to see if two positions are matching with each other"""
+        if not isinstance(obj, Item):
+            self.position = obj
+        else:
+            self.position = obj.position
+        return self.position == self.position
+
+
+N = Item("Needle")
+
+E = Item("Ether")
+
+T = Item("Tube")
+
+
+def main():
+    needle = Item('Needle')
+    print(needle.name, needle.position)
+    print(T.name, T.position)
+    print(E.name, E.position)
+
+if __name__ == "__main__":
+    main()
