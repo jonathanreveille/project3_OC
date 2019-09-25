5cabe43695184a414b1b59fb05530429098ab3cc
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-09-25 12:03:15 +0200

    cleaning

--- macgyver/models/items.py
+++ macgyver/models/items.py
@@ -4,24 +4,23 @@
 """ This module will represent all different items on the map for McGaver"""
 
 class Item:
-
-    """ This class will create objects that are items in the game,
-    those items are going to be positionned randomly on the map """
+    """This class will create objects that are items in the game,
+    those items are going to be positionned randomly on the map"""
 
     def __init__(self, name):
-        """ constructor for item Class objects"""
+        """ Constructor for item Class objects """
         self.name = name
         self.position = None
 
     def __repr__(self):
-        """ method that allow us to see what data has an object from Item Class"""
+        """ Method that allow us to see what data has an object from Item Class """
         return f"Item(position={self.position},name= ({self.name})"
 
     def __str__(self):
         return self.name[0]
 
     def __eq__(self, obj):
-        """ method to see if two positions are matching with each other"""
+        """ Method to see if two positions are matching with each other """
         if not isinstance(obj, Item):
             self.position = obj
         else:
