98cd0ca09dcee0ef768da11444a6f71a507fc89f
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2019-09-25 16:45:42 +0200

    translated in english, and wrote commentaries

--- macgyver/views/macgyver.py
+++ macgyver/views/macgyver.py
@@ -7,42 +7,37 @@ import pygame as pg
 from config.settings import HERO, VELOCITY, SPRITE_WIDTH, SPRITE_HEIGHT
 from macgyver.models.mouvement import right, left, up, down
 
-class MacGyver(pg.sprite.Sprite): #Classe d'héritage de Sprite
-    """ class to add hero on game, record  his moves  and updates"""
+class MacGyver(pg.sprite.Sprite):
+    """ This class inherits from sprite class. This is the class
+    to represent our hero, record his moves and update position """
 
     def __init__(self, hero): #Initializer
         
         super().__init__()  #calling sprites class to initialize object, use of super().__init__()
         self.image = pg.image.load(HERO).convert_alpha() #loading image
-        self.rect = self.image.get_rect() # la position de MacGyver. get_rect = c'est le rectangle de mon image et sa position
+        self.rect = self.image.get_rect() # get_rect = rectangle of the image and its position
         self.hero = hero
 
-
     def update(self):
-        """ Methods that manages and tracks all updates from events"""
+        """ Method that manages and tracks all updates from events"""
         self._process_keyboard_events()
         self.rect.x = self.hero.position.x * SPRITE_WIDTH
         self.rect.y = self.hero.position.y * SPRITE_HEIGHT
 
-
     def _process_keyboard_events(self):
-        """ method that captures events keys from user and it does action for game"""
+        """ Method that captures events keys from gamer and it does action in the game"""
     
         for event in pg.event.get():
             if event.type == pg.KEYDOWN and event.key == pg.K_UP:
-                #self.rect.move_ip(0, -VELOCITY)
                 self.hero.move(up)
             
             elif event.type == pg.KEYDOWN and event.key == pg.K_DOWN:
-                #self.rect.move_ip(0, +VELOCITY)
                 self.hero.move(down)
                 
             elif event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
-                #self.rect.move_ip(-VELOCITY, 0)
                 self.hero.move(left)
             
             elif event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
-                #self.rect.move_ip(VELOCITY, 0)
                 self.hero.move(right)
 
 
